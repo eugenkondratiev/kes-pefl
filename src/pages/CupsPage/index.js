cccb075918f15ebfbee268bfc19e7bc568941f2c
Eugen Kondratiev <kondratiev.es@gmail.com>
2021-10-19 17:00:05 +0300

    cup selects sorted

--- src/pages/CupsPage/index.js
+++ src/pages/CupsPage/index.js
@@ -14,7 +14,7 @@ function CupsPage(props) {
     // const { loading: cupsLoading, cups, getFfCupData, getCups, getFfCups } = useContext(CupsContext);
     // const { loading: cupsLoading, cups } = useContext(CupsContext);
     const cupsContext = useContext(CupsContext);
-    const { loading: cupsLoading, cups, getFfCups , getFfCupData} = cupsContext;
+    const { loading: cupsLoading, cups, getFfCups, getFfCupData } = cupsContext;
 
     const { loading: nationsLoading, getNation, nations } = useContext(NationsContext);
 
@@ -46,11 +46,11 @@ function CupsPage(props) {
             return acc
         }, [])
         console.log(" ######### - federations", federations);
-        setFederations(_federations)
+        setFederations(_federations.sort((a, b) => a[1].localeCompare(b[1])))
     }, [cups])
 
     useEffect(() => {
-        if (!cups) return;
+        if (!cups || !ffId) return;
         const ffCups = getFfCups(ffId);
         console.log("getFfCups --- ", ffCups);
         const ffCupsArray = [];
@@ -62,14 +62,17 @@ function CupsPage(props) {
     }, [ffId]);
 
     useEffect(() => {
-        if (!cups || !cupType) return;
+        if (!cups || !cupType || !ffId) return;
         const _seasons = getFfCupData(ffId, cupType);
         // const seasonsArray = [];
-        
-        
-        setSeasonsList(_seasons.s);
-        console.log("getFfCupData --- ", _seasons, seasonsList);
-        if (!_seasons.s.includes(cupSeason)) setCupSeason(_seasons.s[0])
+
+
+        if (_seasons && _seasons.s) {
+
+            setSeasonsList(_seasons.s.sort((a, b) => +a - +b));
+            console.log("getFfCupData --- ", _seasons, seasonsList);
+            if (!_seasons.s.includes(cupSeason)) setCupSeason(_seasons.s[0])
+        }
 
     }, [ffId, cupType]);
 
@@ -97,7 +100,7 @@ function CupsPage(props) {
         if (value === "extracup" && intFF < 2000) setFfId(intFF + 2000)
         if (value !== "extracup" && intFF > 2000) setFfId(intFF - 2000)
     }
-    const seasonChangeHandler = (value)=>{
+    const seasonChangeHandler = (value) => {
         console.log(`selected  season ${value}`);
         setCupSeason(value)
     }
@@ -118,7 +121,7 @@ function CupsPage(props) {
                         {
                             !cupsLoading && federations && federations.map((_ff) => {
                                 // console.log(" #### OPTION ", _ffId, _ff);
-                                if (_ff[0]) return <Option value={_ff[0]}>{_ff[1]}</Option>
+                                if (_ff[0]) return <Option key={_ff[0]} value={_ff[0]}>{_ff[1]}</Option>
                             })
                         }
                     </Select>
@@ -141,11 +144,11 @@ function CupsPage(props) {
                         value={cupSeason}
                         onChange={seasonChangeHandler}
                     >
-                         {
+                        {
                             !cupsLoading && seasonsList && seasonsList.map((season) => {
                                 return <Option value={season}>{season}</Option>
                             })
-                        } 
+                        }
                     </Select>}
                 </form>
                 }
