9a492fdff74ea2031aaa1e93551ebafb95015784
Eugen Kondratiev <kondratiev.es@gmail.com>
2021-11-24 16:32:08 +0200

    fixed not circled copy2clipboard button

--- src/components/Copy2Clipboard/index.js
+++ src/components/Copy2Clipboard/index.js
@@ -2,14 +2,13 @@ import { getTitle } from 'hookrouter';
 import React from 'react';
 import { SvgCopyIcon } from '../../assets/svg/icons';
 import StyledButton from '../StyledButton';
-import stl from './Copy2Clipboard.module.scss';
+import './Copy2Clipboard.css';
 
-function Copy2Clipboard({ copytext}) {
+function Copy2Clipboard({ copytext }) {
     if (!navigator || !navigator.clipboard.writeText) return null
 
     return (
         <StyledButton
-            className={stl.root}
             onClick={
                 () => {
                     console.log(getTitle())
